1f1ca3638bbfe7f54a94c071cafaba501a52a0a1
Aria Xiying Bao <87568028+ariaxxxi@users.noreply.github.com>
2023-07-23 18:24:40 -0400

    Update movement.cpp

--- movement.cpp
+++ movement.cpp
@@ -4,11 +4,12 @@ for (int i = 0 ; i < nSides ; i++)
 {
     for (int j=0; j < nCopies ; j ++)
   {
-     float angle = (i / nSides) * (3.14 * 2);
-     float2 pos = center + (j / nCopies) * radius * float2(sin(1-angle),
-                                                           cos(1-angle));
+     float angle = (i / nSides) * sin(time * 2) * (3.14 * 2);
+     float2 pos = center + (j / nCopies) * radius * float2(sin(1-angle) - sin(time),
+                                                           cos(1-angle) - sin(time));
      result += length (pos-uv) < size
   }
 }
+outEmissive = result * float3 (sin(time), 0 , 0.1);
 
 return (result)
