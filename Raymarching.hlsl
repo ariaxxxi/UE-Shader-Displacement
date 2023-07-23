5318d129384d616d6e150e571d0cb98e9e98b644
Aria Xiying Bao <87568028+ariaxxxi@users.noreply.github.com>
2023-07-23 18:43:41 -0400

    Create Raymarching.hlsl

---
+++ Raymarching.hlsl
@@ -0,0 +1,18 @@
+// 2DCircle = length(pos-uv);
+
+// 3DSphere = length(rayOtigin - sphereCenter) - sphereRadius;
+
+float3 rayOrigin = viewDir - worldPos;
+float3 rayStep = viewDir * 1;
+
+for (int i =0 ; i < 256 ; i++)
+{
+  float dist = length(rayOrigin - sphereCenter) - sphereRadius;
+  if (dist < 0.0.1) 
+    {
+        return float3(1,0,0); // if inside this sphere, return color red
+    }
+  rayOrigin += rayStep;
+}
+
+return float3(0,0,0); //othereise, return black (empty/transparent)
