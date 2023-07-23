be674e11614081a9aaa968221ddff3539ffc3a1a
Aria Xiying Bao <87568028+ariaxxxi@users.noreply.github.com>
2023-07-23 19:23:02 -0400

    Update displace.hlsl

--- displace.hlsl
+++ displace.hlsl
@@ -5,9 +5,9 @@ float3 lightDirection = normalize(lightPos);
 
 for (int i = 0; i < 256; i++)
 {
-	float displace = sphereCenter + (sin(rayOrigin.x * sin(time)/3) +
-									 sin(rayOrigin.y * sin(time)/3) +
-								   	 sin(rayOrigin.z * sin(time) / 3));
+	float displace = sphereCenter + (sin(rayOrigin.x * sin(time)/3) +  sin(rayOrigin.x * sin(time)/3) +
+									   sin(rayOrigin.y * sin(time)/3) +
+								   	   sin(rayOrigin.z * sin(time)/3));
 	float dist = length(rayOrigin - displace) - sphereRadius;
 
 		if (dist < 0.01)
