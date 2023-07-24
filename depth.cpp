31cfc6f17927ee016033e828c2138fd185fca7dd
Aria Xiying Bao <87568028+ariaxxxi@users.noreply.github.com>
2023-07-23 22:10:42 -0400

    Update depth.cpp

--- depth.cpp
+++ depth.cpp
@@ -1,16 +1,16 @@
 float3 rayStep = cameraDir * -1;
 float4 inputTex = Texture2DSample(texObject, texObjectSampler, uv);
 
-for (int i = 0 ; i < 25 ; i ++)
+for (int i = 0 ; i < depth ; i ++)
 {
   if(inputTex.r > 0.1 && inputTex.g > 0.1 && inputTex.b > 0.1)
   {
-    return float3(i,0,0) //brighter and brighter red: gradient
+    return float3(0,0,i  ); //brighter and brighter red: gradient
   }
 
-  uv += rayStep * 0.15; //distance between the slices
+  uv += rayStep * slices; //distance between the slices
 
-  inputTex = Texture2DSampler(texObject, texObjectSampler, uv.xy);
+  inputTex = Texture2DSample(texObject, texObjectSampler, uv.xy);
 }
 
 return inputTex;
