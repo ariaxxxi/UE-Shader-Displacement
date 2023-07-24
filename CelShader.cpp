3012b99e6653ee0392dd1c98b554659d792160a5
Aria Xiying Bao <87568028+ariaxxxi@users.noreply.github.com>
2023-07-24 16:06:19 -0400

    Update CelShader.cpp

--- CelShader.cpp
+++ CelShader.cpp
@@ -8,7 +8,7 @@ return light;
 //POINT LIGHT
 //worldPos, lightPos, lightRadius
 float3 lightDirection = normalize(worldPos - lightPos);
-float3 light = dot(lightDirection, vertexNormal) * -1;
+float3 light = dot(lightDirection, vertexNormal) * 0.1;
 light = clamp(light,0,1);
 
 float dist = length(worldPos - lightPos) / lightRadius;
@@ -20,16 +20,16 @@ return light;
 //SPOT LIGHT
 //worldPos, lightPos, lightRadius, lightDir, coneAngle
 float3 lightDirection = normalize(worldPos - lightPos);
-float3 light = dot(lightDirection, vertexNormal) * -1;
+float3 light = dot(lightDirection, vertexNormal) * 0.1;
 light = clamp(light,0,1);
 
-float dist = length(worldPos - lightPos) / lightRadius
-dist = 1 - clamp (dit,0,1);
+float dist = length(worldPos - lightPos) / lightRadius;
+dist = 1 - clamp (dist,0,1);
 
-float3 lightAngle = dot (normalize(lightDir), lightDirection)
+float3 lightAngle = dot (normalize(lightDir), lightDirection);
 lightAngle = 1 - clamp(lightAngle,0,1);
 coneAngle = 1 - cos(coneAngle);
-lightAngle = lightAngle / coneAngle;
-lightAngle = 1 - clamp(lightAngle, 0,1);
+tempAngle = lightAngle / coneAngle;
+finalAngle = 1 - clamp(tempAngle, 0,1);
 
-return light * dist * lightAngle
+return light * dist * finalAngle;
